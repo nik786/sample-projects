b7f0a69256bf0c386e4c8814df1836b01730069b
nik786 <chakraborty.rock@gmail.com>
2023-11-15 16:23:32 +0530

    Update p5.py

--- test-2/mavenproject1/p5.py
+++ test-2/mavenproject1/p5.py
@@ -1,12 +1,13 @@
-import yaml, os, argparse
+import os, argparse
+import ruamel.yaml
 from pprint import pprint 
 
 def PreParse():
-    testFile = open('test.yml', 'r')
+    testFile = open('test3.yml', 'r')
     lines = testFile.readlines()
     escapedLines = []
     for line in lines:
-         escapedLines.append(line.replace("'","'\''"))
+         escapedLines.append(line.replace("'","\'\'\'"))
     with open("testEscaped.yml","w+") as out:
         for escapedLine in escapedLines:
              out.write(escapedLine)
@@ -17,13 +18,13 @@ def PostParse():
     outFile.close()
     fixedLines = []
     for line in lines:
-        fixedLines.append(line.replace("\'\'\'","'"))
+        fixedLines.append(line.replace("\'\'\'","\'"))
     outFile = open("output.yml","w+")
     for fixedLine  in fixedLines:
-        outFile.write(fixedLine)
+        outFile.write(fixedLine.replace('"',''))
     outFile.close()
 
-
+runYaml = ruamel.yaml.YAML()
 parser = argparse.ArgumentParser()
 parser.add_argument("-api")
 parser.add_argument("-id")
@@ -32,19 +33,19 @@ parser.add_argument("-v2")
 args = parser.parse_args()
 PreParse()
 
-os.remove("test.yml")
+os.remove("test3.yml")
 testFile = open('testEscaped.yml', 'r')
-parsed = yaml.safe_load(testFile)
+parsed = runYaml.load(testFile)
 testFile.close()
 
 keyToInsert = args.id
-valueToInsert = {'defaultMajorMinorVersion': "\'%s\'"%args.v1, 'versionRules':{1:{'defaultMinorVersion':"\'%s\'"%args.v2}}}
+valueToInsert = {'defaultMajorMinorVersion': '\'%s\''%args.v1, 'versionRules':{1:{'defaultMinorVersion':'\'%s\''%args.v2}}}
 parsed['eurekazuul']['serviceClientVersionMap'][args.api]['clientVersionRules'][keyToInsert] = valueToInsert
 
 outFile = open("output.yml","w+")
-yaml.dump(parsed, outFile)
+runYaml.dump(parsed, outFile)
 outFile.close()
 PostParse()
 print("Data has been added")
 os.remove("testEscaped.yml")
-os.rename("output.yml","test.yml")
+os.rename("output.yml","test3.yml")
